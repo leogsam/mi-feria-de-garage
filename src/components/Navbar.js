fc2fe354ecb6414ca1db92e7bf1d5ba8b0218a4e
Leonardo Gsam <info@leonardogsam.com.ar>
2021-06-08 00:40:51 -0300

    crea app navbar y le da estilo al home

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar(){
+    return(
+        <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Nosotros</li>
+            <li>Forma Parte</li>
+            <li>Contactanos</li>
+        </ul>
+    )
+}
+export default Navbar;
